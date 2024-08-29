4ace58cf60add24ba36d3259421f34044aa6f163
zhangmengmeng <1483472971@qq.com>
2024-08-30 00:01:58 +0800

    Correct capitalization

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import Koa from 'koa';
-import bodyparser from 'koa-bodyparser';
+import bodyParser from 'koa-bodyparser';
 import { context } from './extend';
 import { routes } from './route';
 
@@ -10,7 +10,7 @@ dotenv.config();
 const app = new Koa();
 
 app.use(context); //扩充ctx;
-app.use(bodyparser()); //解析body
+app.use(bodyParser()); //解析body
 app.use(routes); //路由
 app.use(context => {
   context.body = context;
